c91bcb3e47dd99b3d820a3777d1c85aa25a58f28
Rajesh <rajeshtejavath4422@gmail.com>
2025-09-14 01:30:06 +0530

    Roman to Integer

--- leetcode_13.py
+++ leetcode_13.py
@@ -1,3 +1,4 @@
+# 13. Roman to Integer
 # Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
 # Symbol       Value
@@ -8,12 +9,30 @@
 # C             100
 # D             500
 # M             1000
+def romantoint(s):
+    d={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    res=0
+    prev=0
+    for i in range(len(s)-1,-1,-1):
+        value=d[s[i]]
+        if value>=prev:
+            res+=value
+        else:
+            res-=value
+        prev=value
+    return res
 
-text="II"
-res=[]
-d={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-for v in d:
-    for ch in text:
-        if d[v]==ch:
-            res.append(ch)
-print(res)
+
+testcases = [
+    "I",
+    "IV",
+    "IX",
+    "XIV",
+    "LVIII",
+    "MCM",
+    "MCMXCIV",
+    "MMXXV",
+    "MMMCMXCIX"
+]
+for item in testcases:
+    print(item, "---->",romantoint(item))
